238fd110d65b7135dfb8b42f4220e72b15d6fbae
saltrom <65927097+saltrom@users.noreply.github.com>
2021-04-17 00:48:17 +0300

    First Upload
    :)

---
+++ Normal Projects/calculator.py
@@ -0,0 +1,22 @@
+num1 = int(input("Enter Number 1: "))
+num2 = int(input("Enter Number 2: "))
+print("Please Select Action: '+'-'/'*'^^'")
+operation = input()
+result = 0
+if operation == "+":
+    result = num1 + num2
+    print(result)
+elif operation == "-":
+    result = num1 - num2
+    print(result)
+elif operation == "/":
+    result = num1 / num2
+    print(result)
+elif operation == "*":
+    result = num1 * num2
+    print(result)
+elif operation == "^^":
+    result = num1 ** num2
+    print(result)
+else:
+    print("Something went wrong")
